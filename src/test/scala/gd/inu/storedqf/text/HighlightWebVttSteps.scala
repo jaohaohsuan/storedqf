ed742a0f13e86fdcf79d942aaadff9644381b3b0
henry <jaohaohsuan@gmail.com>
2017-04-11 10:35:42 +0800

    add a scenario 'how we style highlight keyword'

--- src/test/scala/gd/inu/storedqf/text/HighlightWebVttSteps.scala
+++ src/test/scala/gd/inu/storedqf/text/HighlightWebVttSteps.scala
@@ -2,7 +2,7 @@ package gd.inu.storedqf.text
 
 import cucumber.api.PendingException
 import cucumber.api.scala.{EN, ScalaDsl}
-import gd.inu.storedqf.format.{CueString, WebVtt}
+import gd.inu.storedqf.format.{CueProperties, CueString, WebVtt}
 import org.json4s.JsonAST.JString
 import org.scalatest.Matchers
 import org.json4s._
@@ -12,6 +12,7 @@ class HighlightWebVttSteps extends ScalaDsl with EN with Matchers {
 
   private var highlightResults: () => Iterable[HighlightFragment] = () => List.empty
   private var vtt: String = ""
+  private var highlightFragment: HighlightFragment = null
 
 
   Given("""^a raw of webvtt like fragment:$"""){ (arg0:String) =>
@@ -28,5 +29,21 @@ class HighlightWebVttSteps extends ScalaDsl with EN with Matchers {
     }
     highlightedVtt should include (arg0)
   }
+
+  Given("""^convert a small piece of highlight result "([^"]*)" to HighlightFragment$"""){ (arg0:String) =>
+    highlightFragment = new HighlightFragment(arg0)
+  }
+  Given("""^we can use CueProperties trait to get cueid "([^"]*)" which will help use to locate cue position in webvtt$"""){ (arg0:String) =>
+    (highlightFragment: CueProperties).cueid(highlightFragment.fragment).value should be (arg0)
+  }
+  Given("""^css class id "([^"]*)" come from the prefix of cueid without dash & numbers$"""){ (arg0:String) =>
+    (highlightFragment: CueProperties).role(highlightFragment.fragment).value should be (arg0)
+  }
+  Given("""^to support css class level styling like this "([^"]*)" we call replace method to get result "([^"]*)"$"""){ (arg0:String, arg1:String) =>
+    highlightFragment.newTag.value should be (arg0)
+    val result = highlightFragment.replace
+    result should be (arg1)
+  }
+
 
 }
